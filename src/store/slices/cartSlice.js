6876737cd320c866c67bf7f79118b40639fb0bd3
Thomas <thomasmadariaga2021@outlook.es>
2025-01-05 18:01:08 -0300

    registro actualizacion carrito

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -18,6 +18,7 @@ const cartSlice = createSlice({
       } else {
         state.value.cart.push(action.payload);
         state.value.total += action.payload.productxCount;
+        state.value.updateAt = Date.now().toLocaleString()
       }
     },
     removeItemToCart: (state, action) => {
@@ -25,6 +26,7 @@ const cartSlice = createSlice({
       state.value.cart = state.value.cart.filter(
         (product) => product.id !== action.payload.id
       );
+      state.value.updateAt = Date.now().toLocaleString()
     },
   },
 });
